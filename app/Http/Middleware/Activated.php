f6165e49388a6397f8b9146750ebc37c4b783e8b
asher-miti <asher_3@hotmail.co.uk>
2021-11-24 22:56:35 +0000

    initial

---
+++ app/Http/Middleware/Activated.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class Activated
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if (!$request->user()?->active()) {
+            return $request->expectsJson()
+
+            ? abort (403, 'Your account is not activated.')
+
+            : redirect()->guest(route('activate'));
+
+        }
+        return $next($request);
+    }
+}
